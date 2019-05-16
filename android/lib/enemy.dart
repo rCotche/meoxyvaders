6893d3c10da4f0895b1c7ebc9af033f9959e4e48
rcotche.cdi <roshan.cotche@gmail.com>
2019-05-16 09:09:59 +0400

    levelup enemy

--- android/lib/enemy.dart
+++ android/lib/enemy.dart
@@ -5,15 +5,17 @@ import 'package:flutter/material.dart';
 
 import 'main.dart';
 
-const SPEED = 120.0;
+
 const ENEMY_SIZE = 40.0;
 class Enemy extends SpriteComponent {
+  double SPEED;
   Size dimensions;
   bool explode = false;
   Enemy(this.dimensions) : super.square(ENEMY_SIZE, 'enemy.png');
   double maxY;
   @override
   void update(double t) {
+    this.levelUP();
     y += gameOver ? 0
         : t * SPEED;
 
@@ -45,8 +47,14 @@ class Enemy extends SpriteComponent {
   }
 
   void levelUP(){
-    if(points >= 5){
-      
+    if(points == 0){
+      SPEED = 120.0;
+    }else{
+      SPEED = 120.0 * (1+0.5*(points~/5));
+    }
+    if(points%5 == 0 && points != 0){
+      SPEED = SPEED * 1.05;
+      print(points%5);
     }
   }
 }
